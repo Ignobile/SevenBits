2f205c93c85d7db3980a7eefd6a5900f3c9601e8
Ignobile <rcastrosalles@gmail.com>
2018-05-22 22:42:35 -0400

    Codigo para el logueo a facebook y mail

---
+++ logueo/Login.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Facebook.Unity;
+
+public class Login : MonoBehaviour {
+
+    public GameObject DialogLoggedIn;
+    public GameObject DialogLoggedOut;
+    public GameObject DialogUsername;
+    public GameObject DialogProfilePic;
+
+
+    List<string> Perms = new List<string>() {"public_profile","email","user_friends" };
+	void Awake () {
+        FB.Init(InitCompleteCallback, UnityCallbackDelegate);
+	}
+    
+
+
+    public void LoginButton()
+    {
+        if (!FB.IsLoggedIn)
+        {
+            FB.LogInWithReadPermissions(Perms, LoginCallback);
+        }
+        else
+        {
+            Debug.Log("El usuario esta logueado");
+        }
+        
+    }
+    
+    
+
+    #region callback
+    private void LoginCallback(IResult result) {
+
+        if(result.Error != null)
+        {
+            Debug.Log(result.Error);
+        }
+        else
+        {
+            if (FB.IsLoggedIn)
+            {
+                Debug.Log("FB is logged in");
+            }
+            else
+            {
+                Debug.Log("FB is not logged in");
+            }
+
+            DealWithFBMenus(FB.IsLoggedIn);
+        }
+        }
+  
+    private void InitCompleteCallback() {
+        if (FB.IsInitialized)
+        {
+            Debug.Log("loguado exitosamente");
+        }
+        else
+        {
+            Debug.Log("fallo");
+        }
+
+        DealWithFBMenus(FB.IsLoggedIn);
+    }
+    private void UnityCallbackDelegate(bool isUnity)
+    {
+        if (isUnity)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+    #endregion
+
+    void DealWithFBMenus(bool isLoggedIn)
+    {
+        if (isLoggedIn)
+        {
+            DialogLoggedIn.SetActive(true);
+            DialogLoggedOut.SetActive(false);
+
+            FB.API("/me?fields=first_name", HttpMethod.GET, DisplayUsername);
+            FB.API("/me/picture?type=square&height=100&width=100", HttpMethod.GET, DisplayProfilePic);
+            
+        }
+        else
+        {
+            DialogLoggedIn.SetActive(false);
+            DialogLoggedOut.SetActive(true);
+
+        }
+    }
+
+    void DisplayUsername(IResult result)  
+    {
+        Text Username = DialogUsername.GetComponent<Text>();
+        if(result.Error == null)
+        {
+            Username.text = "Hola, " + result.ResultDictionary["first_name"];
+        }
+        else
+        {
+            Debug.Log(result.Error);
+        }
+    }
+
+    void DisplayProfilePic(IGraphResult result)
+    {
+        if(result.Texture != null)
+        {
+            Image ProfilePic = DialogProfilePic.GetComponent<Image>();
+            ProfilePic.sprite = Sprite.Create(result.Texture, new Rect(0, 0, 100, 100), new Vector2());
+
+        }
+    }
+    
+}
---
+++ logueo/paneles.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class email : MonoBehaviour {
+
+    public GameObject loginpanel;
+    public GameObject signuppanel;
+
+    public InputField loginEmail;
+    public InputField loginPass;
+
+    public InputField signupEmail;
+    public InputField signupPass;
+    public InputField signupConfirmPass;
+
+
+	// Use this for initialization
+	void Start () {
+
+        ShowLoginPanel();
+		
+	}
+	
+	public void ShowLoginPanel()
+    {
+        ShowPanel(loginpanel);
+    }
+
+    public void ShowSignUpPanel()
+    {
+        ShowPanel(signuppanel);
+    }
+    
+
+    public void ShowPanel(GameObject panel)
+    {
+        loginpanel.SetActive(false);
+        signuppanel.SetActive(false);
+
+        panel.SetActive(true);
+    }
+}
