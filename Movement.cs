fee868d6fc8507ae4a10901a9c62f34faed279d2
cato20 <38285347+cato20@users.noreply.github.com>
2018-05-24 08:12:58 -0400

    Script de movimiento

---
+++ Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Transform myTransform;
+    //Variable de Transform
+
+    public float speed;
+    //Velocidad del personaje
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            myTransform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
+        }
+        //Condicion si se pulsa una tecla, que su eje z vaya en negativo
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            myTransform.Translate(new Vector3(0, 0, -speed) * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            myTransform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            myTransform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+        }
+
+    }
+}
