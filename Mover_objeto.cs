659507a436ce7d4ce08325b4d1dfe696db745733
diego1797 <diegomayorga40@gmail.com>
2018-06-07 10:36:35 -0400

    codigo para mover objetos
    este código sirve para seleccionar un objeto y moverlo por donde tu quieras

---
+++ Mover_objeto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover_objeto : MonoBehaviour {
+    float distance = 10;
+
+    //metodo para agarrar el objeto y arrastrarlo
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+        
+    }
+}
