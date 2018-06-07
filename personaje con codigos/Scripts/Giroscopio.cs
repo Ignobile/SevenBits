787e38458f98da57010fb17608a68842371f1a68
Ignobile <rcastrosalles@gmail.com>
2018-06-06 22:44:21 -0400

    Codigo para el personaje

---
+++ personaje con codigos/Scripts/Giroscopio.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Giroscopio : MonoBehaviour
+{
+
+    public GameObject VRCamaras;
+
+    private float PosicionInicialY = 0f;
+    private float PosicionDelGyroEnY = 0f;
+    private float CalibrarEnLaPosicionY = 0f;
+
+    public bool SeInicioElJuego;
+
+    void Start ()
+    {
+        Input.gyro.enabled = true;
+        PosicionInicialY = VRCamaras.transform.eulerAngles.y;
+	}
+	
+
+	void Update ()
+    {
+        AplicarRotacionDelGyroscopio();
+        AplicarCalibracion();
+
+        if(SeInicioElJuego == true)
+        {
+            Invoke("CalibrarEnPosicionY", 3f);
+            SeInicioElJuego = false;
+        }
+	}
+
+    void AplicarRotacionDelGyroscopio()
+    {
+        VRCamaras.transform.rotation = Input.gyro.attitude;
+        VRCamaras.transform.Rotate(0f, 0f, 180f, Space.Self);
+        VRCamaras.transform.Rotate(90f, 180f, 0f, Space.World);
+        PosicionDelGyroEnY = VRCamaras.transform.eulerAngles.y;
+    }
+
+    void CalibrarEnPosicionY()
+    {
+        CalibrarEnLaPosicionY = PosicionDelGyroEnY - PosicionInicialY;
+    }
+
+    void AplicarCalibracion()
+    {
+        VRCamaras.transform.Rotate(0f, -CalibrarEnLaPosicionY, 0f, Space.World);
+    }
+
+}
---
+++ personaje con codigos/personaje/Mouse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour {
+    Vector2 mouseLook; 
+    Vector2 snoothV; 
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+
+    void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        snoothV.x = Mathf.Lerp(snoothV.x, md.x, 1f / smoothing);
+        snoothV.y = Mathf.Lerp(snoothV.y, md.y, 1f / smoothing);
+
+        mouseLook += snoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+
+
+    }
+}
