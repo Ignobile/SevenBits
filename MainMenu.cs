97f183abadb0119f5b6c523fbb28ca0f490209e2
Rodrigo Zambrana <williamzambrana85@gmail.com>
2018-06-09 14:59:21 -0400

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    //Este metodo es donde hace el cambio de escenas
+    public void SceneChangerDenver()
+    {
+        SceneManager.LoadScene("Aqui poner nombre de escenario de denver", LoadSceneMode.Single);
+    }
+    public void SceneChangerPrime()
+    {
+        SceneManager.LoadScene("Aqui poner nombre de escenario de Prime", LoadSceneMode.Single);
+    }
+    public void SceneChangerRegistrar()
+    {
+        SceneManager.LoadScene("Aqui poner nombre de escenario de Registros", LoadSceneMode.Single);
+    }
+    public void SalirApp()
+    {
+        Debug.Log("Saliendo...");
+        Application.Quit();
+    }
+}
---
+++ MenuOpciones.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOpciones : MonoBehaviour {
+
+    public static bool MenuIsOpen = false;
+    public GameObject SubMenuUI;
+
+    //Este Metodo se ocupa de abrir el sub menu
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            SubMenuUI.SetActive(true);
+            Debug.Log("Key Pressed");
+        }
+        else if(Input.GetKeyDown(KeyCode.F2))
+        {
+            SubMenuUI.SetActive(false);
+            Debug.Log("Key Pressed");
+        }
+
+    }
+
+    //Cambia a la Escnea del Menu Principal
+    public void scenechangerMainMenu()
+    {
+        SceneManager.LoadScene("Menu3D", LoadSceneMode.Single);
+    }
+
+    /*void CloseMenu()
+    {
+        SubMenuUI.SetActive(false);
+        MenuIsOpen = false;
+    }
+    void OpenMenu()
+    {
+        SubMenuUI.SetActive(true);
+        MenuIsOpen = true;
+    }*/
+}
+
